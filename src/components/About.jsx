ee30839132025a41b62ba38441177640a135c735
07ajitdas <07ajitdas@gmail.com>
2024-12-16 07:11:03 -0800

    width changed in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -101,7 +101,7 @@ const About = () => {
             controls
             data-aos="zoom-in"
             data-aos-delay="200"
-            width={400}
+            width={500}
             height={400}
             url='https://www.youtube.com/watch?v=jNv7LM_VoJI'
           />
